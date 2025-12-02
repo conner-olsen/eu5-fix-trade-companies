0943141167a54a770effe06362c755ab6c862915
Conner Olsen <contact@conner-olsen.dev>
2025-12-02 17:09:59 -0500

    move images to assets and update prepare-release

--- prepare-release.sh
+++ prepare-release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-rsync -av --inplace --no-times   --exclude 'dist/'   --exclude '.metadata/'   --exclude '.git/'   --exclude '.gitattributes'   --exclude 'LICENSE'   --exclude 'README.md'   --exclude 'prepare-release.sh'   ./ dist/
+rsync -av --inplace --no-times --exclude 'assets/' --exclude 'dist/' --exclude '.metadata/' --exclude '.git/' --exclude '.gitattributes' --exclude 'LICENSE' --exclude 'README.md' --exclude 'prepare-release.sh' ./ dist/
 
 rm -rf ../fix-trade-companies
 cp -r dist ../fix-trade-companies
