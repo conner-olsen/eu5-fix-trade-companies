5d46fd499a7cc5a3b6910f198ad4ab8002e98c04
Conner Olsen <contact@conner-olsen.dev>
2026-01-08 15:52:34 -0500

    Conform to devkit v0.1

--- prepare-release.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-rsync -av --inplace --no-times in_game main_menu dist/
-
-rm -rf ../fix-trade-companies
-cp -r dist ../fix-trade-companies
-
-find dist -mindepth 1 -maxdepth 1 ! -name '.metadata' -exec rm -rf {} +
